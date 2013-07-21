5935b85b603a34f33b21baadb593fbdeef766fb3
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-07-21 23:36:05 +0300

    Turkish Language Added

---
+++ lang/tr/wardrobe.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Language
+ * @version  4.x
+ * @author   Sinan Eldem <sinan@sinaneldem.com.tr>
+ * @link     http://sinaneldem.com.tr
+ */
+
+return array(
+
+  // Başlık
+  'write' => 'Yaz',
+  'posts' => 'Yazılar',
+  'accounts' => 'Hesaplar',
+  'logout' => 'Çıkış',
+  'login' => 'Giriş',
+
+  // Yazılar
+  'post_title' => 'Başlık',
+  'post_slug' => 'URI Kelimesi',
+  'post_author' => 'Yazar',
+  'post_content' => 'İçeriği buraya yazın...',
+  'post_tags' => 'Taglar',
+  'post_scheduled' => 'Zamanlandı',
+  'post_publish_date' => 'Yayınlama Tarihi',
+  'post_publish_date_set' => 'Ayarla',
+  'post_publish' => 'Yazıyı Yayınla',
+  'post_save' => 'Yazıyı Kaydet',
+  'post_preview' => 'Önizleme',
+  'post_expand' => 'Seçenekler için genişlet',
+  'post_success' => 'Başarılı!',
+  'post_saved' => 'Yazı başarıyla kaydedildi',
+  'post_errors' => '<strong>Hata</strong> Lütfen aşağıdaki hataları çözünüz',
+
+  // Yazılar Listesi
+  'posts_none' => 'Yazı yok',
+  'post_status' => 'Durum',
+  'post_active' => 'Aktif',
+  'post_published' => 'Yayınlandı',
+  'post_draft' => 'Taslak',
+  'post_delete' => 'Yazıyı Sil',
+  'post_delete_confirm' => '##post## yazıyı silmek istediğinizden emin misiniz?',
+
+  // Hesaplar
+  'account_first_name' => 'Ad',
+  'account_last_name' => 'Soyad',
+  'account_email' => 'E-posta',
+  'account_password' => 'Şifre',
+  'account_password_confirm' => 'Şifreyi Onayla',
+  'account_password_keep' => 'Şifrenizi şimdi değiştirmek istemiyorsanız boş bırakın',
+  'account_add' => 'Hesap Ekle',
+  'account_save' => 'Hesabı Kaydet',
+  'account_add_new' => 'Yeni Hesap Ekle',
+  'account_saved' => '<strong>Başarılı!</strong> Hesap kayıt edildi!',
+  'account_delete' => 'Kullanıcı Sil',
+  'account_delete_confirm' => '##first_name## ##last_name##? kullanıcısını silmek istediğinizden emin misiniz?',
+
+  // Giriş
+  'login_email' => 'E-posta',
+  'login_password' => 'Şifre',
+  'login_remember' => 'Beni Hatırla',
+  'login_sign_in' => 'Giriş Yap',
+  'login_incorrect' => 'E-posta adresiniz ve şifreniz hatalı.',
+  'login_forgot' => 'Şifrenizi mi unuttunuz?',
+
+  // Şifremi Unuttum
+  'forgot_password' => 'Şifremi Unuttum',
+  'forgot_sent' => 'Şifre sıfırlama bilgisi içeren bir e-posta gönderidildi.',
+  'forgot_send' => 'Şifre sıfırlama e-postası gönder',
+  'forgot_pass_reset' => 'Şifre Sıfırlama',
+  'forgot_pass_reset_send' => 'Şifre sıfırlama e-postası gönder',
+
+  // Hatalar
+  'error' => 'Hata!',
+  'error_fivehundred' => 'Sunucu iç hatası gerçekleşti.'
+);
